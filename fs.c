713d4fe760f00682ff2adc1bfa236894f4138515
Pedro J. Ruiz Lopez <holzplatten@es.gnu.org>
2013-06-16 16:24:54 +0200

    Licensed under GPLv3. :)

--- fs.c
+++ fs.c
@@ -1,4 +1,4 @@
-/* -*- mode: C -*- Time-stamp: "2013-06-16 12:16:35 holzplatten"
+/* -*- mode: C -*- Time-stamp: "2013-06-16 16:22:16 holzplatten"
  *
  *       File:         fs.c
  *       Author:       Pedro J. Ruiz Lopez (holzplatten@es.gnu.org)
@@ -7,6 +7,26 @@
  *       Funciones genéricas del sistema de archivos.
  *
  */
+
+/*
+  Copyright (C) 2013 Pedro J. Ruiz López <holzplatten@es.gnu.org>
+
+  This file is part of GnordoFS.
+
+  GnordoFS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  GnordoFS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include <fcntl.h>
 #include <stdlib.h>
--- superblock.c
+++ superblock.c
@@ -1,4 +1,4 @@
-/* -*- mode: C -*- Time-stamp: "2013-06-10 22:41:14 holzplatten"
+/* -*- mode: C -*- Time-stamp: "2013-06-16 16:22:44 holzplatten"
  *
  *       File:         superblock.c
  *       Author:       Pedro J. Ruiz Lopez (holzplatten@es.gnu.org)
@@ -7,6 +7,26 @@
  *       Funciones de manejo de superbloque.
  *
  */
+
+/*
+  Copyright (C) 2013 Pedro J. Ruiz López <holzplatten@es.gnu.org>
+
+  This file is part of GnordoFS.
+
+  GnordoFS is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  GnordoFS is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
